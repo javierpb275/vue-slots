f2ef20b7658a513a82daaf268cbe8bfbdbff0d4d
javierpb275 <javierpb275@gmail.com>
2021-10-31 20:08:45 +0100

    created form slot. the slot form is a resusable <form> tag and we can import it and wrap some content with it. we can add a default content in the slot like 'no form to render' in case no content is wrapped inside.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,29 @@
 <template>
-<div>hello</div>
+  <div>
+    <app-form>
+      <div class="help">
+        <p>This is some help text.</p>
+      </div>
+      <div class="fields">
+        <input type="text" placeholder="email" />
+        <input type="text" placeholder="username" />
+        <input type="password" placeholder="password" />
+      </div>
+      <div class="buttons">
+        <button type="submit">Submit</button>
+      </div>
+    </app-form>
+    <app-form></app-form>
+  </div>
 </template>
 
 <script>
+import AppForm from "./components/Form.vue";
+
 export default {
-  name: 'App'
-}
+  name: "App",
+  components: {
+    AppForm,
+  },
+};
 </script>
-
-
